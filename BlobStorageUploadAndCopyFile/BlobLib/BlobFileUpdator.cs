589779289a0470d5a632ea6dea114f18b584c4a1
masahiro aoki <maoki@it-brn.com>
2018-11-04 04:11:46 +0900

    アップロード処理とコピー処理を追加

---
+++ BlobStorageUploadAndCopyFile/BlobLib/BlobFileUpdator.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobLib
+{
+    public class BlobFileUpdator
+    {
+        private readonly CloudBlobContainer _cloudBlobContainer;
+
+        public BlobFileUpdator(string connectionString, string containerName)
+        {
+            if (CloudStorageAccount.TryParse(connectionString, out CloudStorageAccount storageAccount))
+            {
+                var cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                _cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
+            }
+            else
+            {
+                throw new Exception("ConnectionString failed.");
+            }
+        }
+
+        public async Task CreateAndCopyFileAsync(string fileName, string path, Stream file, int allFileCount)
+        {
+            var firstfileName = EditFileName(fileName, 1);
+            var dir = _cloudBlobContainer.GetDirectoryReference(path);
+            var cloudBlockBlob = dir.GetBlockBlobReference(firstfileName);
+
+
+            // upload blob
+            await cloudBlockBlob.UploadFromStreamAsync(file);
+
+
+            // copy blob
+            var numList = Enumerable.Range(2, allFileCount - 1);
+            var tasks = numList.Select(i =>
+            {
+                var name = EditFileName(fileName, i);
+                return CopyBlobAsync(dir, cloudBlockBlob, name);
+            });
+
+            await Task.WhenAll(tasks);
+
+
+            Debug.WriteLine("ok!");
+        }
+
+        
+
+        private async Task CopyBlobAsync(CloudBlobDirectory dir, CloudBlockBlob sourceBlob, string destFileName)
+        {
+            var destBlob = dir.GetBlockBlobReference(destFileName);
+            await destBlob.StartCopyAsync(sourceBlob);
+        }
+
+        private string EditFileName(string fullPathFileName, int num)
+        {
+            var fileName = Path.GetFileName(fullPathFileName);
+            var name = Path.GetFileNameWithoutExtension(fileName);
+            var editName = $"{name}-{num}{Path.GetExtension(fileName)}";
+            return editName;
+        }
+    }
+}
--- BlobStorageUploadAndCopyFile/BlobStorageUploadAndCopyFile/Program.cs
+++ BlobStorageUploadAndCopyFile/BlobStorageUploadAndCopyFile/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using BlobLib;
+using System;
+using System.IO;
 
 namespace BlobStorageUploadAndCopyFile
 {
     class Program
     {
+        private const string StorageConnectionString = ""; // TODO key. push前に削除
+        private const string ContainerName = "samplecontainer";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            args = SetArgDummy(); // TODO debug
+
+            if (!IsValid(args))
+            {
+                Environment.Exit(-1);
+            }
+
+            using (var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+            {
+                var blob = new BlobFileUpdator(StorageConnectionString, ContainerName);
+                blob.CreateAndCopyFileAsync(args[0], args[1], stream, int.Parse(args[2])).Wait();
+            }
+
+        }
+
+        private static bool IsValid(string[] args)
+        {
+            return true; // TODO あとで
+        }
+
+        static string[] SetArgDummy()
+        {
+            // arg0 local file name
+            // arg1 file count
+            return new[] { @"C:\wk\testdata.txt", "test1", "30" };
         }
     }
 }
